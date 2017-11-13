54e7e1312a661db0946c73e4221c7a45f704d571
Tim Miller <timm@uber.com>
2017-11-13 14:40:28 -0800

    add a script to set up dependencies and build a deb package

---
+++ build-deb.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eux
+
+docker run -v $(pwd):/opt/rkt debian:sid /bin/bash -c "\
+  cd /opt/rkt && \
+  ./scripts/install-deps-debian-sid.sh && \
+  ./autogen.sh && \
+  ./configure --with-stage1-flavors=coreos --enable-tpm=no --enable-sdjournal=no && \
+  make && \
+  apt-get install -y ruby ruby-dev && \
+  gem install fpm && \
+  cd /opt/rkt/scripts/pkg && \
+  BUILDDIR=/opt/rkt/build-rkt-1.29.0/ ./build-pkgs.sh 1.29.0 && \
+  mv /opt/rkt/build-rkt-1.29.0/target/bin/rkt_1.29.0-1_amd64.deb /opt/rkt/rkt_1.29.0-1_amd64.deb"
